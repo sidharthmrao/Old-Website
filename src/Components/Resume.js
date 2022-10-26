9a73fb9cd0bac5f1dab1fdb22d07989d00dc200f
Sidharth Rao <sidharthmrao@gmail.com>
2022-10-26 00:42:25 -0400

    ok this is good.

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -2,19 +2,10 @@ import React, { Component } from "react";
 import Slide from "react-reveal";
 
 class Resume extends Component {
-  getRandomColor() {
-    let letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
 
   render() {
     if (!this.props.data) return null;
 
-    const skillmessage = this.props.data.skillmessage;
     const education = this.props.data.education.map(function (education) {
       return (
         <div key={education.school}>
@@ -23,7 +14,7 @@ class Resume extends Component {
             {education.degree} <span>&bull;</span>
             <em className="date">{education.graduated}</em>
           </p>
-          <p>{"Field Team Lead, Builder, Programmer [Choate Robotics Team]\nDean's List [All Terms]\nChoate Cross Country JV\nVice President [Choate Aerospace Association]"}</p>
+          <p>{education.description}</p>
         </div>
       );
     });
@@ -38,20 +29,6 @@ class Resume extends Component {
           </p>
           <p>{work.description}</p>
         </div>
-      );
-    });
-
-    const skills = this.props.data.skills.map((skills) => {
-      const backgroundColor = this.getRandomColor();
-      const className = "bar-expand " + skills.name.toLowerCase();
-      const width = skills.level;
-      const image = skills.image;
-
-      return (
-        <li key={skills.name}>
-          <span style={{ width, backgroundColor }} className={className}></span>
-          <em>{skills.name}</em>
-        </li>
       );
     });
 
@@ -82,24 +59,6 @@ class Resume extends Component {
             </div>
 
             <div className="nine columns main-col">{work}</div>
-          </div>
-        </Slide>
-
-        <Slide left duration={1300}>
-          <div className="row skill">
-            <div className="three columns header-col">
-              <h1>
-                <span>Skills</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">
-              <p>{skillmessage}</p>
-
-              <div className="bars">
-                <ul className="skills">{skills}</ul>
-              </div>
-            </div>
           </div>
         </Slide>
       </section>
