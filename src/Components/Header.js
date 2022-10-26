5c15e66674d039a88a96e7eea51f1a54b80eab60
Sidharth Rao <sidharthmrao@gmail.com>
2022-10-26 10:12:28 -0400

    Changed links to open in new tab.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -61,10 +61,10 @@ class Header extends Component {
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
+                <a href={project} target="_blank" rel="noopener noreferrer" className="button btn project-btn">
                   <i className="fa fa-linkedin-square"></i>LinkedIn
                 </a>
-                <a href={github} className="button btn github-btn">
+                <a href={github} target="_blank" rel="noopener noreferrer" className="button btn github-btn">
                   <i className="fa fa-github"></i>Github
                 </a>
               </ul>
