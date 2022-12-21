9cb422d4179ad18c208bfff6365d225c6d2be268
Sidharth Rao <sidharthmrao@gmail.com>
2022-12-21 00:39:43 -0500

    Updated resume pdf file.

--- src/Components/About.js
+++ src/Components/About.js
@@ -47,7 +47,7 @@ class About extends Component {
                 <div className="columns download">
                   <p>
                     <a href={resume} target="_blank" rel="noopener noreferrer" className="button">
-                      <i className="fa fa-linkedin-square"></i>LinkedIn Resume
+                      <i className="fa fa-save"></i>Resume
                     </a>
                   </p>
                 </div>
